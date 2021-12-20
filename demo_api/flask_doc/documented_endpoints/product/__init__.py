f4aef6393b0974d8c07cfdd7c40fd00062dfad42
KvlingxSS <72736664+caohieu-281@users.noreply.github.com>
2021-12-21 00:42:53 +0700

    Kha ok

--- demo_api/flask_doc/documented_endpoints/product/__init__.py
+++ demo_api/flask_doc/documented_endpoints/product/__init__.py
@@ -65,7 +65,6 @@ class ShowProduct(Resource):
     @namespace.response(500, 'Internal Server error')
     @namespace.response(404, 'Not Found')
 
-
     def get(self):
         con = sqlite3.connect('database.db')
         cur = con.cursor()
@@ -93,9 +92,6 @@ class SearchByName(Resource):
         cur = con.cursor()
         name = request.args.get('name') #name = ao
         if(name is None):
-            # response = app.response_class(response=json.dumps("Invalid value"),
-            #                         status=200,
-            #                         mimetype='error')
             return 400
         cur.execute("SELECT * FROM products WHERE name LIKE \"%{}%\"".format(name))
         fetchdata = cur.fetchall()
@@ -103,9 +99,6 @@ class SearchByName(Resource):
         response = responses(fetchdata)
         if(len(fetchdata) == 0):
             return 404
-            # response = app.response_class(response=json.dumps("not found"),
-            #                         status=200,
-            #                         mimetype='error')
         
         return response
 
@@ -117,40 +110,34 @@ def is_float(element) -> bool:
         return False
 
 
+parser_price = reqparse.RequestParser()
+parser_price.add_argument('from', type=str, help='Price product\'s from (eg: 100)')
+parser_price.add_argument('to', type=str, help='Price product\'s to (eg: 200)')
+
 @namespace.route('/price')
 class SearchByPrice(Resource):
 
-    @namespace.marshal_list_with(product_model)
     @namespace.response(500, 'Internal Server error')
     @namespace.response(404, 'Not Found')
     @namespace.response(400, 'Invalid value')
     @namespace.response(250,'from and to must be numberic')
     @namespace.response(251,'from must be less than to')
-
+    @namespace.expect(parser_price)
     def get(self):
         con = sqlite3.connect('database.db')
         cur = con.cursor()
         froms = request.args.get('from') 
         tos = request.args.get('to') 
         if(froms is None or tos is None):
-            # response = app.response_class(response=json.dumps("Invalid value"),
-            #                         status=200,
-            #                         mimetype='error')
             response = 400
             # return response
         if(not is_float(tos) or not is_float(froms)):
-            # response = app.response_class(response=json.dumps("from and to must be numberic"),
-            #                         status=200,
-            #                         mimetype='error')
             response = 250
             return response
             
         tos = float(tos)
         froms = float(froms)
         if(tos < froms):
-        #     response = app.response_class(response=json.dumps("from must be less than to"),
-        #                             status=200,
-        #                             mimetype='error')
             response = 251
             return response
         cur.execute("SELECT * FROM products WHERE {} > price AND price > {}".format(tos, froms))
@@ -159,18 +146,20 @@ class SearchByPrice(Resource):
         response = responses(fetchdata)
         if(fetchdata == ()):
             response = 404
-        #     response = app.response_class(response=json.dumps("not found"),
-        #                             status=200,
-        #                             mimetype='error')
         return response
 
+
+parser_filter = reqparse.RequestParser()
+parser_filter.add_argument('type', type=str, help='Product\'s type (eg: mem muot)')
+parser_filter.add_argument('size', type=str, help='Product\'s size (eg: A)')
+parser_filter.add_argument('color', type=str, help='Product\'s color (eg: vang)')
 @namespace.route('/filters')
 class SearchByFilters(Resource):
 
-    @namespace.marshal_list_with(product_model)
+    @namespace.doc(product_model)
     @namespace.response(500, 'Internal Server error')
     @namespace.response(404, 'Not Found')
-
+    @namespace.expect(parser_filter)
     def get(self):
         con = sqlite3.connect('database.db')
 
@@ -207,24 +196,20 @@ class SearchByFilters(Resource):
 
         if(len(fetchdata) == 0):
             response = 404
-        #     response = app.response_class(response=json.dumps("not found"),
-        #                             status=200,
-        #                             mimetype='error')
         return response
 
 
 parser_add = reqparse.RequestParser()
 parser_add.add_argument('id', type=int, help='Product\'s id (eg: 123)', location='form')
-parser_add.add_argument('name', type=str, help='Product\'s name (eg: quan)', location='form')
+parser_add.add_argument('name', type=str, help='Product\'s name (eg: quan dai)', location='form')
 parser_add.add_argument('type', type=str, help='Product\'s type (eg: quan)', location='form')
-parser_add.add_argument('price', type=float, help='Product\'s price (eg: quan)', location='form')
-parser_add.add_argument('description', type=str, help='Product\'s description (eg: quan)', location='form')
-parser_add.add_argument('size', type=str, help='Product\'s size (eg: quan)', location='form')
-parser_add.add_argument('image', type=str, help='Product\'s image (eg: quan)', location='form')
-parser_add.add_argument('video', type=str, help='Product\'s video (eg: quan)', location='form')
-parser_add.add_argument('color', type=str, help='Product\'s color (eg: quan)', location='form')
-parser_add.add_argument('quantity', type=int, help='Product\'s quantity (eg: quan)', location='form')
-
+parser_add.add_argument('price', type=float, help='Product\'s price (eg: 50000)', location='form')
+parser_add.add_argument('description', type=str, help='Product\'s description (eg: vai den tron bong)', location='form')
+parser_add.add_argument('size', type=str, help='Product\'s size (eg: M)', location='form')
+parser_add.add_argument('image', type=str, help='Product\'s image (eg: fgfg)', location='form')
+parser_add.add_argument('video', type=str, help='Product\'s video (eg: dfg)', location='form')
+parser_add.add_argument('color', type=str, help='Product\'s color (eg: den)', location='form')
+parser_add.add_argument('quantity', type=int, help='Product\'s quantity (eg: 5)', location='form')
 @namespace.route('/add_product', methods=['PUT'])
 class AddProduct(Resource):
 
@@ -249,12 +234,7 @@ class AddProduct(Resource):
         cur.execute("INSERT INTO products (ID, name, type, price, description, size, image, video, color, quantity) VALUES ({}, \"{}\", \"{}\", {}, \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", {});".format
                     (id, name, type, price, description, size, image, video, color, quantity))
         con.commit()
-
         cur.close()
-        # response = app.response_class(response=json.dumps("successfully added"),
-        #                             status=200,
-        #                             mimetype='notification')
-
         response = 200
         return response
 
@@ -279,12 +259,8 @@ class DeleteProduct(Resource):
         con.commit()
 
         cur.close()
-        # response = app.response_class(response=json.dumps("successfully delete"),
-        #                             status=200,
-        #                             mimetype='notification')
         response = 202
         return response
-
 
 @namespace.route('/edit_product', methods=['POST'])
 class EditProduct(Resource):
@@ -302,11 +278,6 @@ class EditProduct(Resource):
             id = request.form['id']
         except:
             return 401
-        # if(id is None):
-        #     # response = app.response_class(response=json.dumps("id not NULL"),
-        #     #                         status=200,
-        #     #                         mimetype='error')
-        #     return 
         
         name = request.form.get('name', default=None)
         type = request.form.get('type', default=None)
@@ -323,9 +294,6 @@ class EditProduct(Resource):
         fetchdata = cur.fetchall()
         
         if(len(fetchdata) == 0):
-            # response = app.response_class(response=json.dumps("ID not found"),
-            #                         status=200,
-            #                         mimetype='error')
             response = 402
             cur.close()
             return response
